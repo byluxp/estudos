fbfae7a585488b53d54b452eda7573fecb9ba217
byluxp <lucila.vitoria@outlook.com.br>
2026-04-08 22:27:37 -0300

    simulacao saque de banco

--- python/exercicios/ex10-while.py
+++ python/exercicios/ex10-while.py
@@ -1,9 +1,17 @@
 valor = 0
 opçao = ''
-escolhas = 0
+escolhas = maior = menor = 0
 while opçao != 'S':
     num = int(input('Digite um valor: '))
     opçao = input('Deseja sair? [S/N] ').upper()
     valor += num
     escolhas += 1
+    if escolhas == 1:
+        maior = menor = num
+    else:
+        if num > maior:
+            maior = num
+        if num < menor:
+            menor = num
 print('A soma dos valores é: {}. Você digitou {} números. E a média dos valores é {}'.format(valor, escolhas, valor/escolhas))
+print ('O maior valor é {} e o menor valor é {}'.format(maior, menor))
