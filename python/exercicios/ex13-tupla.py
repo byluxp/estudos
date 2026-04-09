c117a14be8e0b22ce69a094bf7b45026fea7db03
Lucila <lucila.vitoria@outlook.com.br>
2026-04-09 16:46:12 +0000

    tabela do brasileirao

---
+++ python/exercicios/ex13-tupla.py
@@ -0,0 +1,5 @@
+tabelaBrasileirao = (1,2,3,4,5,6,7,8,9,10)
+
+print(tabelaBrasileirao[0:6]) #mostra todos de 0 a 5
+print(tabelaBrasileirao[6:]) #mostra todos a partir da posição 6 (no caso numero 7)
+print(tabelaBrasileirao.index(7)) #mostra em que posição está o número 7
