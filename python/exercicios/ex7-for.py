83396dbfc3e3046f1ca3b6680847d2089bd8dd26
byluxp <lucila.vitoria@outlook.com.br>
2026-04-08 21:33:13 -0300

    exercicio fatorial

--- python/exercicios/ex7-for.py
+++ python/exercicios/ex7-for.py
@@ -2,8 +2,8 @@ y = 0
 for c in range(1,6):
     s = int(input('Digite um valor: '))
     if s % 2 == 0:
-       s = 0
+       s = 0 # transformar o valor em 0 para não ser somado
     else:
-        y += s
-    print (y)
-print('Soma: ', y)
+        y += s # y se torna y + s, ou seja, vai somando os valores ímpares digitados pelo usuário
+    print (y) # imprime o valor de y a cada iteração
+print('Soma: ', y) #imprime o valor total de y
