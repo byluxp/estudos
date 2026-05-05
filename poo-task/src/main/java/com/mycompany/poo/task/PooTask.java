1d120a4a2911e5c338dba08da9f296a2dcd9d0a9
byluxp <lucila.vitoria@outlook.com.br>
2026-05-04 22:06:35 -0300

    pooTask1

---
+++ poo-task/src/main/java/com/mycompany/poo/task/PooTask.java
@@ -0,0 +1,13 @@
+package com.mycompany.poo.task;
+public class PooTask {
+
+    public static void main(String[] args) {
+        ContaBanco CB1 = new ContaBanco();
+        CB1.abrirConta(CC);
+        CB1.setSaldo(15);
+        CB1.exibirStatus();
+        CB1.Sacar(20);
+        CB1.exibirStatus();
+        
+    }
+}
